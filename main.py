25d8b6cc4f8abf4acffeadc90bb456f45b34d275
olidec <oliver.decapitani@gmail.com>
2022-01-18 15:34:05 +0000

    edited main

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello World!")
+from bs4 import BeautifulSoup 
+from requests import get 
+
+url="https://xkcd.com/archive/"
+res=get(url)
+soup=BeautifulSoup(res.content, "html.parser")
+hrefs=[elem['href'] for elem in soup.select(r"a[title]")] 
+print(hrefs)
